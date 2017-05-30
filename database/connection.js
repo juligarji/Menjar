712bf0332614498ec713690a8264e019b07919f0
juligarji <jgarzonj@unal.edu.co>
2017-05-30 11:23:12 -0500

    Subir archivos, mantener sesion y cambios interfaz

--- database/connection.js
+++ database/connection.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://menj_test:menj_test@ds157641.mlab.com:57641/menjar_test');
+//mongoose.connect('mongodb://menj_test:menj_test@ds157641.mlab.com:57641/menjar_test');
 
-//mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://localhost/test');
 var db = mongoose.connection;
 
 module.exports = db;
--- public/js/login/provider/controller.js
+++ public/js/login/provider/controller.js
@@ -1,56 +1,5 @@
 
  //FUNCIONES PREDEFINIDAS ANTERIORMENTE
-/*
-  function createProduct(){
-
-
-    var sentData = JSON.stringify({
-
-          //img:$("input[name='img']").val(),
-          //img:document.getElementById('img').files[0],
-          name:$("input[name='name']").val(),
-          category:$("select[name='category']").val(),
-          expirationDate:$("input[name='expirationDate']").val(),
-          price:parseFloat($("input[name='price']").val()),
-
-          location:{
-            lat:  parseFloat($('#location').attr('lat')),
-            lng: parseFloat($('#location').attr('lng'))
-          },
-
-          description:$("textarea[name='description']").val()
-
-    });
-
-
-    var call = $.ajax({
-          type:'POST',
-          url:'/provider/new-product',
-          //dataType:'json',
-          //contentType: 'application/json',
-          contentType: 'multipart/form-data',
-          cache: false,
-          //contentType: false,
-          processData: false,
-
-          data:sentData,
-          sucess : function(data){
-            console.log('sucess executed');
-          }
-    });
-
-    call.done(function(data){
-
-        //if(data==null) return;
-
-    });
-
-    call.fail(function(jqXHR, textStatus, error){
-        console.log('<error>: ' + jqXHR.responseJson + error + textStatus);
-    });
-
-  }
-*/
 
 function createProduct(){
 
@@ -65,13 +14,6 @@ function createProduct(){
   myFormData.append('price',$("input[name='price']").val());
   myFormData.append('lat',$('#location').attr('lat'));
   myFormData.append('lng',$('#location').attr('lng'));
-    /*var obj = {
-      "lat":  parseFloat($('#location').attr('lat')),
-      "lng": parseFloat($('#location').attr('lng'))
-    };*/
-
-    //var strConv = JSON.stringify(obj);
-
 
  myFormData.append('description',$("textarea[name='description']").val());
 
@@ -98,7 +40,7 @@ function createProduct(){
 
   call.fail(function(jqXHR, textStatus, error){
       console.log('<error>: ' + jqXHR.responseJson + error + textStatus);
-      //location.reload();
+      location.reload();
   });
 
 }
@@ -152,84 +94,3 @@ function uploadPicture(){
      });
  }
   }
-
-
-  /* nUEVAS FUNCIONES */
-
-
-  /*
-  $(document).ready(function() {
-
-   $('#uploadForm').submit(function() {
-      $("#status").empty().text("File is uploading...");
-      $(this).ajaxSubmit({
-
-          error: function(xhr) {
-      //status('Error: ' + xhr.status);
-      console.log('Error ' + xhr.status);
-          },
-
-          success: function(response) {
-      $("#status").empty().text(response);
-              console.log(response);
-          }
-  });
-      //Very important line, it disable the page refresh.
-  return false;
-  });
-});
-*/
-
-/* Solucion terciario */
-/*
-function createProduct(){
-
-  var myFormData = new FormData();
-  myFormData.append('pictureFile',img.files[0]);
-
-  var sentData = {
-
-        //img:$("input[name='img']").val(),
-        img:myFormData,
-        name:$("input[name='name']").val(),
-        category:$("select[name='category']").val(),
-        expirationDate:$("input[name='expirationDate']").val(),
-        price:parseFloat($("input[name='price']").val()),
-
-        location:{
-          lat:  parseFloat($('#location').attr('lat')),
-          lng: parseFloat($('#location').attr('lng'))
-        },
-
-        description:$("textarea[name='description']").val()
-
-  };
-
-
-  var call = $.ajax({
-        type:'POST',
-        url:'/provider/new-product',
-        //dataType:'json',
-        //contentType: 'application/json',
-        processData: false, // important
-        contentType: false, // important
-        dataType : 'json',
-        data: sentData
-
-        sucess : function(data){
-          console.log('sucess executed');
-        }
-  });
-
-  call.done(function(data){
-
-      //if(data==null) return;
-
-  });
-
-  call.fail(function(jqXHR, textStatus, error){
-      console.log('<error>: ' + jqXHR.responseJson + error + textStatus);
-  });
-
-}
-*/
